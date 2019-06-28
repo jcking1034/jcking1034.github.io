3ba91f057371f7e1a0acaca8ca99c979b737bcfd
James King <jcking1034@gmail.com>
2019-06-28 00:05:34 -0700

    Updates

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function scrollDetect() {
+    if (window.pageYOffset > 0) {
+        document.getElementsByTagName("nav")[0].style.display = "block"
+    } else {
+        document.getElementsByTagName("nav")[0].style.display = "none"
+
+    }
+}
